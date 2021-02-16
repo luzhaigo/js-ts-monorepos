de652b3dc1d494e1a6ef1a0c4f4a777443a8904e
pohsun <po-hsun@ampostech.com>
2021-02-16 22:16:27 +0800

    docs: getTeambyId

--- packages/data/src/teams.ts
+++ packages/data/src/teams.ts
@@ -1,14 +1,12 @@
-import { apiCall } from "@shlack/utils";
-import { isTeam, isTypedArray, ITeam } from "@shlack/types";
+import { apiCall } from '@shlack/utils';
+import { isTeam, isTypedArray, ITeam } from '@shlack/types';
 
 let cachedAllTeamsList: Promise<ITeam[]>;
 export async function getAllTeams(): Promise<ITeam[]> {
-  if (typeof cachedAllTeamsList === "undefined")
-    cachedAllTeamsList = apiCall("teams").then((rawData: unknown) => {
+  if (typeof cachedAllTeamsList === 'undefined')
+    cachedAllTeamsList = apiCall('teams').then((rawData: unknown) => {
       if (isTypedArray(rawData, isTeam)) return rawData;
-      throw new Error(
-        `Unexpected value for teams array\n${JSON.stringify(rawData)}`
-      );
+      throw new Error(`Unexpected value for teams array\n${JSON.stringify(rawData)}`);
     });
 
   return await cachedAllTeamsList;
@@ -16,16 +14,16 @@ export async function getAllTeams(): Promise<ITeam[]> {
 
 const cachedTeamRecords: Record<string, Promise<ITeam>> = {};
 
+/**
+ * Get a {@link @shlack/types#ITeam} by ID
+ * @param id id of team to fetch
+ */
 export async function getTeamById(id: string): Promise<ITeam> {
   let cached = cachedTeamRecords[id];
-  if (typeof cached === "undefined")
-    cached = cachedTeamRecords[id] = apiCall(`teams/${id}`).then(
-      (rawData: unknown) => {
-        if (isTeam(rawData)) return rawData;
-        throw new Error(
-          `Unexpected value for team\n${JSON.stringify(rawData)}`
-        );
-      }
-    );
+  if (typeof cached === 'undefined')
+    cached = cachedTeamRecords[id] = apiCall(`teams/${id}`).then((rawData: unknown) => {
+      if (isTeam(rawData)) return rawData;
+      throw new Error(`Unexpected value for team\n${JSON.stringify(rawData)}`);
+    });
   return await cached;
 }
